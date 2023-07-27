126ffb9ff2fca5762038aef38e63073574c8aeb2
unknown <zulfikrimirza@yahoo.com>
2023-07-27 14:52:14 +0700

    animation

--- src/Profile.js
+++ src/Profile.js
@@ -10,9 +10,14 @@ export default function Profile() {
     return (
         <>
             <section data-aos="fade-down" data-aos-duration="1500" className="profile">
-
+                <div className='notes'>
+                        <p>all resource assets are from Google, if you feel offended because i use your art/what you made, i am sorry
+                            <br/>feel free to email me on zulfikrimirza3@gmail.com, i will remove it immediately
+                        </p>
+                </div>
                 <div className = "profile__content">
-                <h1 className="profile__title">ZULFIKRI MIRZA</h1>
+                    <h1 className="profile__title">ZULFIKRI MIRZA</h1>
+
                 </div>
             </section>
         </>
